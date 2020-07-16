e30c1300735289548b09acd8d5fe0c7c660d04e5
joaocongio <joaocongio@outlook.com>
2020-07-16 18:58:43 -0300

    atualização do novo arquivo

---
+++ funcoes_construtoras.js
@@ -0,0 +1,33 @@
+let Carro = function (){ // criando uma função construtora
+    this.cor = 'Amarelo'
+    this.modelo = 'Chevette'
+    this.velocidadeAtual = 0
+    this.velocidadeMaxima = 180
+
+    this.acelerar = function(){ // this.acelerar é também um atributo, a partir desse atributo podemos executar a função
+       // this.velocidadeAtual += 10
+
+       let velocidade = this.getVelocidadeAtual() + 10 // retorna a velocidade atual e adiciona + 10 na variavel velocidade
+            this.setVelocidadeAtual(velocidade) // pega a variavel velocidade e passa como parametro para o "método" set
+    } 
+
+    this.getVelocidadeAtual = function(){
+        return this.velocidadeAtual
+    }
+
+    this.setVelocidadeAtual = function(velocidadeAtual){
+        this.velocidadeAtual = velocidadeAtual // velocidadeAtual é referente ao parametro passado, esta variavel está disponível apenas do bloco dessa função. Ela que vai receber o valor de velocidade
+            if(velocidadeAtual > 180){
+                console.log('nao')
+            }
+    }
+}
+
+let carro = new Carro() // instanciando a função Carro, será construida de fato e convertida em um obj
+
+console.log(`A velocidade atual é: ${carro.getVelocidadeAtual()}`)
+
+carro.acelerar() // recebe +10
+carro.acelerar() // recebe +10
+
+console.log(`A velocidade agora é de: ${carro.setVelocidadeAtual()}`)
